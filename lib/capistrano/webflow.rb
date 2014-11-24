d043acc73397bc642bd69f3d352d0c21fd11104e
Florian Aman <fa@webflow.de>
2014-11-24 22:08:41 +0100

    * fixed load path

--- lib/capistrano/webflow.rb
+++ lib/capistrano/webflow.rb
@@ -1,2 +1,2 @@
-load File.expand_path("../capistrano/tasks/base.rake", __FILE__)
-load File.expand_path("../capistrano/tasks/mysql.rake", __FILE__)
+load File.expand_path("../tasks/base.rake", __FILE__)
+load File.expand_path("../tasks/mysql.rake", __FILE__)
